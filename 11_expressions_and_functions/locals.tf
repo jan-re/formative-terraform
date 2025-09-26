531abbc074780a1583b9bf7aaa9ace8b0510fd1c
jan-re <rehanekjan@gmail.com>
2025-09-26 08:57:49 +0200

    Starting with expressions

---
+++ 11_expressions_and_functions/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  math       = 2 * 2
+  equality   = 2 != 2
+  comparison = 2 < 1
+  logical    = true || false
+  negation   = !true
+}
---
+++ 11_expressions_and_functions/outputs.tf
@@ -0,0 +1,9 @@
+output "operators" {
+  value = {
+    math       = local.math
+    equality   = local.equality
+    comparison = local.comparison
+    logical    = local.logical
+    negation   = local.negation
+  }
+}
